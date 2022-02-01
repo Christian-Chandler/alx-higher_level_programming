d00d813880833d20e96f8e85667cffa2187f67b4
Christian-Chandler <cccchimaobi@gmail.com>
2022-02-01 03:16:04 +0000

    Safe Print Division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 3-safe_print_division.py
+# CCC
+
+
+def safe_print_division(a, b):
+    '''divides 2 integers and prints the result.'''
+    try:
+        quo = a / b
+    except (ZeroDivisionError, TypeError):
+        quo = None
+    finally:
+        print("Inside result : {}".format(quo))
+    return quo
